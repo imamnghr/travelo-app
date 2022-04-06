1a7bcab82699ed30f644683f1e0405b467220a10
Imam 1654. Nugroho <EXTImam1654@btn.co.id>
2022-04-06 15:15:17 +0700

    Testimonial Card

--- src/containers/Home/SecondSection/Testimonials/index.tsx
+++ src/containers/Home/SecondSection/Testimonials/index.tsx
@@ -1,9 +1,16 @@
+import React from "react";
+
 const Testimonials = () => {
   return (
-    <div className="">
+    <>
+      <h3 className="text-heading-3 font-label font-bold pt-14 mb-1">
+        Testimonial Pelanggan
+      </h3>
+      <p className="text-heading-4 mb-4">
+        Pendapat Pelanggan Tentang Kami
+      </p>
+    </>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Testimonials
+export default Testimonials;
